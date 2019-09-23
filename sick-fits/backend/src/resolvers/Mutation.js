d805f57021a99284e06b1678ec23e0f2de90d1ed
Ollie Philpott <ophilpott@zonedigital.com>
2019-09-23 18:13:05 +0100

    Adding data up to module #5

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,43 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO check if they are logged in
 
-module.exports = mutations;
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+
+    return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    // take a copy of the updates
+    const updates = { ...args };
+    // remove id
+    delete updates.id;
+    // run update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // find item
+    const item = await ctx.db.query.item({ where }, `{ id title }`);
+    // check if own item
+    // TODO
+    // delete
+    return ctx.db.mutation.deleteItem({ where }, info);
+  }
+};
+
+module.exports = Mutations;
